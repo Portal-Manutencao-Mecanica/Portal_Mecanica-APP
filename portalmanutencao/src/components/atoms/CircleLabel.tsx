ed383ba536df0b4c70312c280278ecfbaa6837a1
Arthur Mourão <arthur_b_mourao@estudante.sesisenai.org.br>
2026-07-17 14:21:14 -0300

    Wrap CircleLabel content in a div element

--- portalmanutencao/src/components/atoms/CircleLabel.tsx
+++ portalmanutencao/src/components/atoms/CircleLabel.tsx
@@ -4,6 +4,7 @@ import { CircleLabelProps } from "@/props/CircleLabelProps"
 export default function CircleLabel({color, height, width}:CircleLabelProps){
 
     return(
-        <></>
+        <div>
+        </div>
     );
 }
