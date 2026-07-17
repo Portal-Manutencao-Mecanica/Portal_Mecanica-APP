028677be2477a5a46ad8addc7625175d076e37a1
Arthur Mourão <arthur_b_mourao@estudante.sesisenai.org.br>
2026-07-17 14:20:07 -0300

    Update CircleLabel.tsx

--- portalmanutencao/src/components/atoms/CircleLabel.tsx
+++ portalmanutencao/src/components/atoms/CircleLabel.tsx
@@ -4,8 +4,6 @@ import { CircleLabelProps } from "@/props/CircleLabelProps"
 export default function CircleLabel({color, height, width}:CircleLabelProps){
 
     return(
-        <div className={`${color} ${height} ${width} rounded-full `}>
-                
-        </div>
+        <></>
     );
 }
