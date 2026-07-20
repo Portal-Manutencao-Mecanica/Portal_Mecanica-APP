261f9ab887190f7b3d01447a764546cac007adbd
ARTHUR BATISTA MOURÃO <arthur_b_mourao@estudante.sesisenai.org.br>
2026-07-20 14:23:02 -0300

    feat(component) Creating sidebar

--- portalmanutencao/src/app/page.tsx
+++ portalmanutencao/src/app/page.tsx
@@ -1,10 +1,5 @@
-import { SideBar } from "@/components/organisms/SideBar";
-
 export default function Home() {
     return (
-        <>
-        <SideBar/>
-        
-        </>
+        <></>
     );
 }
