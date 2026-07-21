1e3ae5b3bbe37bf5a68328109d0ad98d982fb414
Aluno SENAI <Aluno@SN-402482.edu.fiesc.com.br>
2026-07-21 09:38:24 -0300

    fix(components): fixes header ux

--- portalmanutencao/src/app/page.tsx
+++ portalmanutencao/src/app/page.tsx
@@ -1,5 +1,9 @@
+import Header from "@/components/organisms/Header";
+
 export default function Home() {
     return (
-        <></>
+        <div>
+            <Header />
+        </div>
     );
 }
