0a840fbadde73c65780dfdf69feda773b5811159
ARTHUR BATISTA MOURÃO <arthur_b_mourao@estudante.sesisenai.org.br>
2026-07-22 08:28:46 -0300

    fix(component) fix login background image

--- portalmanutencao/src/app/maquinas/page.tsx
+++ portalmanutencao/src/app/maquinas/page.tsx
@@ -1,50 +1,50 @@
-"use client";
-
-import Pagination from "@/components/molecules/Pagination";
-import { useState, useEffect } from "react";
-
-export default function ListaDeProdutos() {
-  const [page, setPage] = useState(1);
-  const [allUsers, setAllUsers] = useState<any[]>([]); 
-  const limit = 1; 
-
-  
-  useEffect(() => {
-    fetch("http://localhost:8080/admin")
-      .then((res) => {
-        if (!res.ok) throw new Error("Erro ao buscar dados");
-        return res.json();
-      })
-      .then((data) => setAllUsers(data))
-      .catch((err) => console.error(err));
-  }, []);
-
-  
-  const total = allUsers.length; 
-  const startIndex = (page - 1) * limit;
-  const endIndex = startIndex + limit;
-  
-  
-  const currentResults = allUsers.slice(startIndex, endIndex);
-
-  return (
-    <div>
-      
-      <ul>
-        {currentResults.map((p: any) => (
-          <li key={p.id}>{p.nome}</li>
-        ))}
-      </ul>
-
-      
-      {total > 0 && (
-        <Pagination
-          page={page}
-          limit={limit}
-          total={total}
-          onPageChange={(newPage: number) => setPage(newPage)}
-        />
-      )}
-    </div>
-  );
-}
+//"use client";
+//
+//import Pagination from "@/components/molecules/Pagination";
+//import { useState, useEffect } from "react";
+//
+//export default function ListaDeProdutos() {
+//  const [page, setPage] = useState(1);
+//  const [allUsers, setAllUsers] = useState<any[]>([]); 
+//  const limit = 1; 
+//
+//  
+//  useEffect(() => {
+//    fetch("http://localhost:8080/admin")
+//      .then((res) => {
+//        if (!res.ok) throw new Error("Erro ao buscar dados");
+//        return res.json();
+//      })
+//      .then((data) => setAllUsers(data))
+//      .catch((err) => console.error(err));
+//  }, []);
+//
+//  
+//  const total = allUsers.length; 
+//  const startIndex = (page - 1) * limit;
+//  const endIndex = startIndex + limit;
+//  
+//  
+//  const currentResults = allUsers.slice(startIndex, endIndex);
+//
+//  return (
+//    <div>
+//      
+//      <ul>
+//        {currentResults.map((p: any) => (
+//          <li key={p.id}>{p.nome}</li>
+//        ))}
+//      </ul>
+//
+//      
+//      {total > 0 && (
+//        <Pagination
+//          page={page}
+//          limit={limit}
+//          total={total}
+//          onPageChange={(newPage: number) => setPage(newPage)}
+//        />
+//      )}
+//    </div>
+//  );
+//}
