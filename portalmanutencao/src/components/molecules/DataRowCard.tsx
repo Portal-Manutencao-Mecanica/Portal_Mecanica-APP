5b97a7c9f88b3c493e7e6b1999b4530af7c8f947
ARTHUR BATISTA MOURÃO <arthur_b_mourao@estudante.sesisenai.org.br>
2026-07-23 07:59:47 -0300

    feat(component) Fix props and component data row

--- portalmanutencao/src/components/molecules/DataRowCard.tsx
+++ portalmanutencao/src/components/molecules/DataRowCard.tsx
@@ -1,27 +1,17 @@
-"use client";
-
-import Link from "next/link";
-import Button from "../atoms/Button";
 import { DataRowCardProps } from "@/props/DataRowCardProps";
 
-export function DataRowCard({
-  acronym,
-  professors,
-  classGroupId,
-}: DataRowCardProps) {
+export function DataRowCard({ children, actions }: DataRowCardProps) {
   return (
-    <div className="flex items-center justify-between w-full rounded-xl border border-gray-200 bg-white px-6 py-5 shadow-sm hover:shadow-md transition-all">
-      <div className="flex flex-col gap-1">
-        <span className="text-xl font-semibold text-gray-800">{acronym}</span>
-
-        <span className="text-sm text-gray-500">Professores</span>
-
-        <span className="text-gray-700">{professors.join(", ")}</span>
+    <div className="flex items-center justify-between py-5 px-6 w-340 max-w-7xl bg-weg-card-white rounded-lg shadow-md ">
+      
+      <div className="flex items-center gap-10">
+        {children}
       </div>
 
-      <Link href={`/turmas/${classGroupId}`}>
-        <Button>Ver alunos</Button>
-      </Link>
+      
+      <div className="flex items-center gap-5">
+        {actions}
+      </div>
     </div>
   );
 }
