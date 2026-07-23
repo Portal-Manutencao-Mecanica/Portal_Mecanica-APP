e9b897bdd50854ffd1cdc67d2c164d8fa4cea327
ARTHUR BATISTA MOURÃO <arthur_b_mourao@estudante.sesisenai.org.br>
2026-07-23 11:16:26 -0300

    feat(component) create form maintence

--- portalmanutencao/src/hooks/useAuth.ts
+++ portalmanutencao/src/hooks/useAuth.ts
@@ -5,16 +5,14 @@ export function useAuth() {
     const [user, setUser] = useState<UserProfileProps | null>(null);
 
     useEffect(() => {
-        
         const storedUser = localStorage.getItem("@App:user");
         
         if (storedUser) {
             setUser(JSON.parse(storedUser));
         } else {
-            
             const mockUser: UserProfileProps = {
                 id: 1,
-                name: "João Silva",
+                name: "Arthur Mourão",
                 email: "joao@aluno.com",
                 role: "ALUNO",
             };
