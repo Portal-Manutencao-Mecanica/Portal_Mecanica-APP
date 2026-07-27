26cec976d1e8904cf2edb84a2fd8af54f450e09b
Junior Antunes <jugabriel2612@gmail.com>
2026-07-27 11:26:30 -0300

    fix(confirm-dialog): change props

--- portalmanutencao/src/props/ConfirmDialogProps.ts
+++ portalmanutencao/src/props/ConfirmDialogProps.ts
@@ -2,6 +2,10 @@ export interface ConfirmDialogProps {
   open: boolean;
   title: string;
   description: string;
+
+  confirmText?: string;
+  confirmVariant?: "primary" | "secondary" | "warning" | "danger";
+
   onCancel: () => void;
   onConfirm: () => void;
 }
