4d7c9f708ea3fe3d7e875f4727c90196ce5d87a5
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-28 11:12:10 -0300

    feat: add user data to config page

--- portalmanutencao/src/app/config/page.tsx
+++ portalmanutencao/src/app/config/page.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import LayoutDesktop from "@/components/templates/LayoutDesktop";
 import ConfigForm from "@/components/organisms/ConfigForm";
 
 export default function ConfigPage() {
+
+    const user = {
+        name: "Alexandre Santos",
+        email: "alexandre@weg.net",
+    };
+
     return (
         <LayoutDesktop>
-            <ConfigForm />
+            <ConfigForm user={user} />
         </LayoutDesktop>
     );
 }
