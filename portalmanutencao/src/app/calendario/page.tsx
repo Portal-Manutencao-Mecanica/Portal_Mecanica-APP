a7bdee96e4932c32c9baa106416271b642e7b694
Junior Antunes <jugabriel2612@gmail.com>
2026-07-28 09:37:20 -0300

    feat(calendar): incomplete calendar

--- portalmanutencao/src/app/calendario/page.tsx
+++ portalmanutencao/src/app/calendario/page.tsx
@@ -1,10 +1,34 @@
+"use client"
 import LayoutDesktop from "@/components/templates/LayoutDesktop";
 import Calendar from "@/components/organisms/Calendar";
 
-export default function calendarioPage() {
+const events = [
+  {
+    id: "1",
+    title: "Troca de óleo",
+    start: "2026-07-28T08:00:00",
+  },
+  {
+    id: "2",
+    title: "Inspeção",
+    start: "2026-07-30T14:00:00",
+  },
+  {
+    id: "3",
+    title: "Limpeza",
+    start: "2026-08-02",
+  },
+];
+
+export default function CalendarioPage() {
   return (
     <LayoutDesktop>
-      <Calendar />
+      <Calendar
+        events={events}
+        onEventClick={(info) => {
+          console.log(info.event);
+        }}
+      />
     </LayoutDesktop>
   );
 }
