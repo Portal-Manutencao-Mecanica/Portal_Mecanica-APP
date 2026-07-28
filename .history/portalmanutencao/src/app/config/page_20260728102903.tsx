4f35b049a0392be47bd994be6d862e50b325d03a
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-28 10:29:47 -0300

    feat: add layout desktop to configpage

--- .history/portalmanutencao/src/app/config/page_20260728102903.tsx
+++ .history/portalmanutencao/src/app/config/page_20260728102903.tsx
@@ -1,10 +1,8 @@
 import ConfigForm from "@/components/organisms/ConfigForm"
 import LayoutDesktop from "@/components/templates/LayoutDesktop"
-
 export default function ConfigPage() {
     return (
-        <LayoutDesktop>
-            <ConfigForm />
-        </LayoutDesktop>
+        <LayoutDesktop></LayoutDesktop>
+        <ConfigForm />
     )
 }
--- .history/portalmanutencao/src/app/config/page_20260728102907.tsx
+++ .history/portalmanutencao/src/app/config/page_20260728102907.tsx
@@ -1,6 +1,5 @@
 import ConfigForm from "@/components/organisms/ConfigForm"
 import LayoutDesktop from "@/components/templates/LayoutDesktop"
-
 export default function ConfigPage() {
     return (
         <LayoutDesktop>
