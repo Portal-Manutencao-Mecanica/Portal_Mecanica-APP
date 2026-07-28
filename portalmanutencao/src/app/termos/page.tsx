77f11d86906fbf9369e2b38f881de61cdbd5fc4c
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-28 13:29:25 -0300

    feat: fill termspage with the layout and the termsofuseform

--- portalmanutencao/src/app/termos/page.tsx
+++ portalmanutencao/src/app/termos/page.tsx
@@ -1,5 +1,10 @@
-export default function TermsOfUsePage() {
+import LayoutDesktop from "@/components/templates/LayoutDesktop";
+import TermsOfUseForm from "@/components/organisms/TermsOfUseForm";
+
+export default function TermsPage() {
     return (
-        
-    )
+        <LayoutDesktop>
+            <TermsOfUseForm />
+        </LayoutDesktop>
+    );
 }
