39c0c5705bc709d727b99ac10524bbe8b46b57bf
Junior Antunes <jugabriel2612@gmail.com>
2026-07-28 09:37:20 -0300

    feat(calendar): incomplete calendar

--- portalmanutencao/src/components/organisms/Calendar.tsx
+++ portalmanutencao/src/components/organisms/Calendar.tsx
@@ -1,13 +1,29 @@
 "use client";
 
 import FullCalendar from "@fullcalendar/react";
-
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
-export default function Calendar() {
+import { EventClickArg, EventInput } from "@fullcalendar/core";
+
+interface CalendarProps {
+  events: EventInput[];
+  onEventClick?: (info: EventClickArg) => void;
+}
+
+export default function Calendar({
+  events,
+  onEventClick,
+}: CalendarProps) {
   return (
-    <div className="w-230  mx-auto">
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" height={740} />
+    <div className="w-230 mx-auto">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        height={740}
+        events={events}
+        eventClick={onEventClick}
+      />
     </div>
   );
 }
