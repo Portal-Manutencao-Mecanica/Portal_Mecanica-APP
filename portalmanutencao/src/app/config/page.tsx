ebc1d6b14a4e295b2d60e1f5e27d5bb7978f5612
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-28 11:09:59 -0300

    feat: add content to configpage

---
+++ portalmanutencao/src/app/config/page.tsx
@@ -0,0 +1,10 @@
+import LayoutDesktop from "@/components/templates/LayoutDesktop";
+import ConfigForm from "@/components/organisms/ConfigForm";
+
+export default function ConfigPage() {
+    return (
+        <LayoutDesktop>
+            <ConfigForm />
+        </LayoutDesktop>
+    );
+}
