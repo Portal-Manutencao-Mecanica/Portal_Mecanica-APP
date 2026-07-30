63711ba72222cc1d0c64526f4f33f198c0aa6a49
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-30 07:53:57 -0300

    feat: fill material complementar page with the content of complementar material form

--- portalmanutencao/src/app/maquinas/material-complemetar/page.tsx
+++ portalmanutencao/src/app/maquinas/material-complemetar/page.tsx
@@ -1,5 +1,10 @@
-export default function MaterialComplementar() {
+import LayoutDesktop from "@/components/templates/LayoutDesktop";
+import ComplementarMaterialForm from "@/components/organisms/ComplementarMaterialForm";
+
+export default function ComplementarMaterialPage() {
     return (
-        
-    )
+        <LayoutDesktop>
+            <ComplementarMaterialForm />
+        </LayoutDesktop>
+    );
 }
