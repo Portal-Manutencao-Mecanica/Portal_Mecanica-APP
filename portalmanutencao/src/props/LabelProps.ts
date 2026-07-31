c1d733435f1e7e4194aa77473743315653be0c2e
Junior Antunes <jugabriel2612@gmail.com>
2026-07-31 07:33:47 -0300

    feat(page): add props

--- portalmanutencao/src/props/LabelProps.ts
+++ portalmanutencao/src/props/LabelProps.ts
@@ -1,11 +1,7 @@
-import React from "react"
-import { CircleLabelProps } from "./CircleLabelProps"
 import { LabelStatus } from "@/types/LabelStatus";
 
-export interface LabelProps{
+export interface LabelProps {
     status: LabelStatus;
     text: string;
-    size?: "sm" | "md" | "lg";
+    size?: "sm";
 }
-
-export type { LabelStatus };
